923b7a82fc8da9b43f32a8fbf465378b4f43ce18
Olga Vitek <o.vitek@neu.edu>
2023-01-10 21:11:17 -0500

    Init

---
+++ S23/build_site.R
@@ -0,0 +1,8 @@
+#Set our working directory.
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("/Users/olga/Documents/GitHub/CS3950/S23")
+
+#render your sweet site. 
+rmarkdown::render_site()
